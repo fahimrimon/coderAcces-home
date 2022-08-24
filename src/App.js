3ef148947f24982b83dc2c1fa95e70306a7c90ea
Fahim Rimon <fahimrimon21@gmail.com>
2022-08-25 00:21:25 +0600

    home page complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Certification from "./components/Certification/Certification";
+import Environment from "./components/Environment/Environment";
+import Explore from "./components/Exploring/Explore";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Preparation from "./components/Preparation/Preparation";
+import ReadyMessage from "./components/ReadyMessage/ReadyMessage";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/banner" element={<Banner></Banner>}></Route>
+        <Route path="/explores" element={<Explore></Explore>}></Route>
+        <Route path="/preparation" element={<Preparation></Preparation>}></Route>
+        <Route path="/certification" element={<Certification></Certification>}></Route>
+        <Route path="/testimonials" element={<Testimonials></Testimonials>}></Route>
+        <Route path="/environment" element={<Environment></Environment>}></Route>
+        <Route path="/ready" element={<ReadyMessage></ReadyMessage>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
